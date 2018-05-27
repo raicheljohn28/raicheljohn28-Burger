a57db902aee84c32558d59ee9f1150fb3a64b64e
Raichel John <raicheljohn28@gmail.com>
2018-05-27 16:30:08 -0400

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Double Bacon Cheeseburger'),
+ ('Triple-Stacked Avocado Burger'),
+ ('Cali-Chicken Cheeseburger'),
+ ('Southern Style Veggie Burger'),
+ ('Spicy Pepper Jack Mushroom Burger'),
+ ('Tequila Lime Burger');
+
