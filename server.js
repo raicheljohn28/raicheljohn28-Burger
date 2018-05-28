e526da06cd0434b676df4e1ff764f4596294c51f
Raichel John <raicheljohn28@gmail.com>
2018-05-28 08:55:29 -0400

    up

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -19,7 +19,13 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
-app.listen(port);
+// app.listen(port);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 
